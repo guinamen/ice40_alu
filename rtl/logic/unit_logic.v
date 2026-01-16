0b4e57b831c683d2b2ee422e2b389f93ee156064
Guilherme Namen Pimenta <guilherme.namen@gmail.com>
2026-01-16 09:34:44 -0300

    Refactor unit_logic module to remove c_inv
    Removed the c_inv input and simplified the output assignment.

---
+++ rtl/logic/unit_logic.v
@@ -0,0 +1,13 @@
+// --- Unidade Lógica (Máscaras + XOR) ---
+module unit_logic #(parameter WIDTH = 8) (
+    input  c_and,
+    input  c_or,
+    input  c_xor,
+    input  [WIDTH-1:0] a,
+    input  [WIDTH-1:0] b,
+    output [WIDTH-1:0] out
+);
+    out = ({WIDTH{c_and}} & (a & b)) |
+          ({WIDTH{c_or }} & (a | b)) |
+          ({WIDTH{c_xor}} & (a ^ b));
+endmodule
