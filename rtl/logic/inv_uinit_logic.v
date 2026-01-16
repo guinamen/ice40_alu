a1c0097f49b68694d38cde3fb3632d310995bddb
Guilherme Namen Pimenta <guilherme.namen@gmail.com>
2026-01-16 09:43:45 -0300

    Refactor logic unit instantiation in inv_uinit_logic

--- rtl/logic/inv_uinit_logic.v
+++ rtl/logic/inv_uinit_logic.v
@@ -1,4 +1,3 @@
-// --- Unidade Lógica (Máscaras + XOR) ---
 module inv_uinit_logic #(parameter WIDTH = 8) (
     input  c_and,
     input  c_or,
@@ -9,7 +8,13 @@ module inv_uinit_logic #(parameter WIDTH = 8) (
     output [WIDTH-1:0] out
 );
   wire [WIDTH-1:0] res_base;
-  logic_unit(.c_and(c_and), .c_or(c_or), c_xor(c_xor),.a(a),.b(b),.out(res));
+  unit_logic #(.WIDTH(WIDTH)) logic_unit(
+    .c_and(c_and),
+    .c_or(c_or),
+    .c_xor(c_xor),
+    .a(a),
+    .b(b),
+    .out(res_base));
     // Inversão condicional rápida via XOR
   assign out = res_base ^ {WIDTH{c_inv}};
 endmodule
